2572c43162606b9cd0b4bce25aa7a6a32f5524cf
Agustin <aguspulido14@gmail.com>
2021-03-20 04:38:40 -0300

    Logica terminada

--- src/App.js
+++ src/App.js
@@ -1,39 +1,6 @@
-/*import logo from './logo.svg';
-import './App.css';
-
-function App() {
-
-  $.ajax({
-    url: 'https://randomuser.me/api/',
-    dataType: 'json',
-    success: function(data) {
-      console.log(data);
-    }
-  });
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;*/
-
 import React, { Component } from "react"
+import './App.css';
+import ReactDOM from 'react-dom';
 
 class App extends Component {
 
@@ -60,28 +27,50 @@ class App extends Component {
     var { items, loading } = this.state;
     if (!loading) {
       return (
-        <div>Loading...</div>
+        <div > Loading... </div>
       )
     } else {
       return (
-        <div className="container">
-          <div className="tarjeta row justify-content-center d-flex align-items-center">
-            {items.map(item => (
-              <div className="userInfo col-lg-3">
-                <div className="avatar-small justify-content-center d-flex align-items-center">
-                  <img className="rounded-circle" src={item.picture.thumbnail} alt={item.name.first} />
-                </div>
-                <div className="userInfo row justify-content-center d-flex text-center">
-                  <p>Nombre: {item.name.first} {item.name.last}</p>
-                  <p>Ciudad: {item.location.city}</p>
-                  <p>Nacionalidad: {item.nat}</p>
-                </div>
+        <div className="container" >
+          <div className="row justify-content-center d-flex align-items-center" > {items.map(item => (<div className="col-lg-3" >
+            <div className="user" >
+              <div className="avatar justify-content-center d-flex align-items-center" >
+                <img type="button" onClick={() => this.ShowData(item)} className="rounded-circle" src={item.picture.thumbnail} alt={item.name.first} />
               </div>
-            ))}
+              <div className="userInfo row justify-content-center d-flex text-center" >
+                <p> Name: {item.name.first} {item.name.last} </p>
+                <p> City: {item.location.city} </p>
+                <p> Nationality: {item.nat} </p>
+              </div>
+            </div>
           </div>
+          ))
+          } </div>
         </div>
       )
     }
   }
+  ShowData = (item) => {
+    ReactDOM.render(
+      < React.StrictMode >
+        <div className="container" >
+          <div className="row justify-content-center d-flex align-items-center" >
+            <div className="user" >
+              <div className="avatar justify-content-center d-flex align-items-center" >
+                <img className="rounded-circle" src={item.picture.large} alt={item.name.first} />
+              </div>
+              <div className="userInfo row justify-content-center d-flex text-center" >
+                <p> Name: {item.name.first} {item.name.last} </p>
+                <p> Username: {item.login.username} </p>
+                <p> Email adress: {item.email} </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  }
 }
+
 export default App;
